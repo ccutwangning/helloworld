2838fa6856dab690994af1818e07e3f7efa37013
Colin Wang <1043505663@qq.com>
2016-09-09 11:54:22 +0800

    second

--- src/main/java/com/demo/netty/App.java
+++ src/main/java/com/demo/netty/App.java
@@ -4,10 +4,8 @@ package com.demo.netty;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!6666");
+	}
 }
